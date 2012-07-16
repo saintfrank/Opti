a649f7ed5b8d8c1001274701c199e39dd4972372
Frankie <showandtell@Showandtell.local>
2012-07-16 12:37:57 +0200

    Patt meglio

--- patterns.py
+++ patterns.py
@@ -1,13 +1,21 @@
 from pymprog import *  # Import the module
 
 # index and data
-xid, rid = range(10), range(1)
+xid, rid = range(10), range(2)
 
-c = (6, 6, 4, 12, 7, 3, 5, 2, 2, 5) 
+# Funzione obiettivo
+coverage = (6, 6, 4, 12, 7, 3, 5, 2, 2, 5) 
 
-mat = [ (5, 3, 9, 2, 6, 4, 1, 1, 8, 6)  ]   
+## Mi faccio una matrice che mi fa selezionare
+mat = 	[
+		(5, 3, 9, 2, 6, 4, 1, 1, 8, 6), # Modellazione della somma dei volumi
+		(0, 0, 0, 0, 1, 0, 1, 0, 0, 0), # Ogni riga mi rappresenta un costraint di mutua esclusione(padre/figlio) 
+		(0, 1, 0, 0, 0, 0, 1, 0, 0, 0), # ...
+		(0, 0, 0, 1, 0, 0, 1, 0, 0, 0),
+		(1, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+	]   
 
-b = (30.0, 600.0, 300.0)
+b = (30.0, 1)
 
 #problem definition
 beginModel('basic')
@@ -16,7 +24,7 @@ x = var(xid, 'X', bool) #create variables
 
 # Funzione di massimo 
 maximize( #set objective
-  sum(c[i]*x[i] for i in xid), 'myobj'
+  sum(coverage[i]*x[i] for i in xid), 'myobj'
 )
 
 #print 'xid' 
@@ -32,7 +40,7 @@ r=st(	#set constraints
 
 solve() #solve and report
 
-
+# Stampa del risultato
 print "Solver status:", status()
 print 'Z = %g;' % vobj()  # print obj value
 #Print variable names and primal values
