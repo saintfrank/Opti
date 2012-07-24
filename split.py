f966e55724dba2d0939726c3889b4edc94e64ac3
Frankie <showandtell@Showandtell.local>
2012-07-24 23:05:01 +0200

    Abbellito

--- split.py
+++ split.py
@@ -1,6 +1,5 @@
+from tools import *
 import pymprog      # Import the module
-
-
 
 ## Ora, secondo me, non direi di farlo punto punto, ma traccia traccia. 
 ## Allora prendo le variabili booleane Y[0], Y[1], Y[2], 
@@ -23,36 +22,6 @@ import pymprog      # Import the module
 ## - in generale ho un vettore di variabili x. Il problema di ottimizzazione P e' del tipo
 ##  min { c(x) | Ax<= b }   dove A e b sono vettori di numeri reali
 ##
-
-def print_values_matrix( tracks ):
-	print "%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s" % ( 
-		croce(tracks[0]),  croce(tracks[1]),  croce(tracks[2]),  croce(tracks[3]), 
-		croce(tracks[4]),  croce(tracks[5]),  croce(tracks[6]),  croce(tracks[7]), 
-		croce(tracks[8]),  croce(tracks[9]),  croce(tracks[10]), croce(tracks[11]), 
-		croce(tracks[12]), croce(tracks[13]), croce(tracks[14]), croce(tracks[15]) )
-
-def print_variables_matrix_cross( tracks ):
-    print"%s%s%s%s\n%s%s%s%s\n%s%s%s%s\n%s%s%s%s" % ( 
-        croce(tracks[0].primal),  croce(tracks[1].primal),  croce(tracks[2].primal),  croce(tracks[3].primal), 
-        croce(tracks[4].primal),  croce(tracks[5].primal),  croce(tracks[6].primal),  croce(tracks[7].primal), 
-        croce(tracks[8].primal),  croce(tracks[9].primal),  croce(tracks[10].primal), croce(tracks[11].primal), 
-        croce(tracks[12].primal), croce(tracks[13].primal), croce(tracks[14].primal), croce(tracks[15].primal) )
-
-
-def croce( val ):
-    if val >= 1  :
-        return "x"
-    else:
-        return "_"
-
-
-def print_variables_matrix_primal( tracks ):
-    print"%s %s %s %s\n%s %s %s %s\n%s %s %s %s\n%s %s %s %s" % ( 
-         (tracks[0].primal),  (tracks[1].primal),  (tracks[2].primal),  (tracks[3].primal), 
-         (tracks[4].primal),  (tracks[5].primal),  (tracks[6].primal),  (tracks[7].primal), 
-         (tracks[8].primal),  (tracks[9].primal),  (tracks[10].primal), (tracks[11].primal), 
-         (tracks[12].primal), (tracks[13].primal), (tracks[14].primal), (tracks[15].primal) )
-
 
 
 
@@ -97,9 +66,11 @@ def split ( myTracks ):
     #
     # # # # # # #
     
+    totY = pymprog.var( range(1), 'totY', bounds=(1, 3) )
+    totW = pymprog.var( range(1), 'totW', bounds=(1, 3) )
     
-    #r += pymprog.st( sum ( Y[k] for k in range(3) ) >= 1 )
-    #r += pymprog.st( sum ( W[k] for k in range(3) ) >= 1 )
+    r += pymprog.st( sum ( Y[k] for k in range(3) ) == totY[i] for i in range(1) )
+    r += pymprog.st( sum ( W[k] for k in range(3) ) == totW[i] for i in range(1) )
     
     
     
@@ -140,19 +111,31 @@ def split ( myTracks ):
     
     
     print("Total Volume = %g"% pymprog.vobj())
-    
+   
+    print 'Y'
     print Y 
+    print 'W'
     print W
     
     print AmpY
     print AmpW
     
+    print 'AY'
     print_variables_matrix_primal(AY)
+    print 'AW'
     print_variables_matrix_primal(AW)
-    
+ 
+    print 'Y'
     print_variables_matrix_cross(AY)
+    print 'W'
     print_variables_matrix_cross(AW)
 
+    print 'Volume Y : ' 
+    print VolTotY[0].primal
+    print 'Volume W : ' 
+    print VolTotW[0].primal
+    print 'Volume Tot : ' 
+    print sum( tracks[i] for i in range(N*M) )
 
 
 
@@ -189,16 +172,18 @@ track_2 = (	1, 0, 0, 0,
 print 'Track_2:'
 print_values_matrix(track_2)
 
-track_3 = (	0, 0, 1, 0, 
-		0, 0, 1, 0, 
-		0, 1, 0, 0,  
-		0, 1, 0, 0)
+track_3 = (	0, 0, 0, 1, 
+		0, 0, 0, 1, 
+		0, 0, 0, 1,  
+		0, 0, 0, 1)
 
-myTracks = [track_1, track_2, track_3 ]
 
 print 'Track_3:'
 print_values_matrix(track_3)
 
+
+
+myTracks = [track_1, track_2, track_3 ]
 split(myTracks)
 
 # Funzione di massimo
